525f49564a0d56a40671a77f2b7d07fc00a4f2c2
lennygin <lennginzan@gmail.com>
2020-03-24 15:40:29 -0600

    update

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 int main(){
-    //crea grafo (casita)
     node * root;
     root=create_graph();
       printf("Yeii\n");
-    //encontrar ruta optima
-    start(root);
+    start();
 
 }
--- path_planning_algorithm.h
+++ path_planning_algorithm.h
@@ -1,36 +1,80 @@
-
-typedef struct Table{
+typedef struct Table
+{
   int id;
   int short_distance;
   int Previous_vertex;
-}table;
+} table;
 
-void start(node * root){
-  table * row_A, * row_B, * row_C, * row_D, * row_E;
-  int unvisited[5]={0,1,2,3,4},visited[5]={-1,-1,-1,-1,-1};
-  printf("%d, %d, %d, %d, %d \n",unvisited[0],unvisited[1],unvisited[2],unvisited[3],unvisited[4] );
+void stupid();
+
+void start()
+{
+  table *row_A, *row_B, *row_C, *row_D, *row_E;
+  node *root;
+  int unvisited[5] = {-1, 1, 2, 3, 4}, visited[5] = {0, -1, -1, -1, -1};
+
   row_A->id = 0;
-  //printf("%d\n",row_A->id);
-  row_A->short_distance=0;
-  //printf("%d\n",row_A->short_distance);
-  row_A->Previous_vertex=0;
-  //printf("%d\n",row_A->Previous_vertex);
-  row_B -> id = 1;
-  row_B ->short_distance=32768;
-  row_B -> Previous_vertex=-1;
-  row_C -> id = 2;
-  row_C ->short_distance=32768;
-  row_C -> Previous_vertex=-1;
-  row_D -> id = 3;
-  row_D ->short_distance=32768;
-  row_D -> Previous_vertex=-1;
-  row_E -> id = 4;
-  row_E ->short_distance=32768;
-  row_E -> Previous_vertex=-1;
-  printf("ok\n");
+  row_A->short_distance = 0;
+  row_A->Previous_vertex = 0;
+  row_B->id = 1;
+  row_B->short_distance = 32768;
+  row_B->Previous_vertex = 0;
+  row_C->id = 2;
+  row_C->short_distance = 32768;
+  row_C->Previous_vertex = 0;
+  row_D->id = 3;
+  row_D->short_distance = 32768;
+  row_D->Previous_vertex = 0;
+  row_E->id = 4;
+  row_E->short_distance = 32768;
+  row_E->Previous_vertex = 0;
 
-  //for(int i=0;i<4;i++){
+  root = create_graph();
 
-  //}
+  switch (root->id)
+  {
+  case 0:
+    stupid(root, row_A, &visited, &unvisited);
+    printf("Dentro de A");
+    break;
 
+  case 1:
+    stupid(root, row_B, &visited, &unvisited);
+    printf("Dentro de B");
+    break;
+
+  case 2:
+    stupid(root, row_C, &visited, &unvisited);
+    printf("Dentro de C");
+    break;
+
+  case 3:
+    stupid(root, row_D, &visited, &unvisited);
+    printf("Dentro de D");
+    break;
+
+  case 4:
+    stupid(root, row_E, &visited, &unvisited);
+    printf("Dentro de E");
+    break;
+  }
+}
+
+void stupid(node *graph, table *row, int *visited[5], int *unvisited[5])
+{
+  int i, j, temp[2][5];
+  
+  printf("%d blaaa\n", *graph->nextn[i]->id);
+
+  while (graph->nextn[i] != NULL)
+  {
+    for (j = 0; j < 5; j++)
+      if (graph->nextn[i] == unvisited[j]){
+        temp[j][0] = graph->cost[i];
+        temp[j][1] = graph->nextn;
+        printf("Costo %d \n", graph->cost[i]);
+        printf("id %d \n", graph->nextn);
+      }
+  i++;
+  }
 }
